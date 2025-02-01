3535d14764222c34da10fdf9631ffe79dce7e51b
achibhossengit <mail.achibhossen@gmail.com>
2025-02-01 11:30:49 +0600

    move templates folder in tasks app and undo settings dir path

--- task_management2/settings.py
+++ task_management2/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'task_management2.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [

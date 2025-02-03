52d99ff0ece356d2f0e66e8534b664cc5e75d512
achibhossengit <mail.achibhossen@gmail.com>
2025-02-03 23:02:49 +0600

    add task from ui with using post method

--- tasks/views.py
+++ tasks/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from tasks.forms import TaskForm
-from tasks.models import Employee
+from tasks.models import Employee, Task
 
 # Create your views here.
 def home(request):
@@ -36,5 +36,24 @@ def test_file(request):
 def create_task(request):
     employees = Employee.objects.all()
     form = TaskForm(employees = employees)
+
+
+    if request.method == 'POST':
+        form = TaskForm(request.POST, employees = employees)
+        if form.is_valid():
+            data = form.cleaned_data
+            title = data.get('title')
+            description = data.get('description')
+            due_date = data.get('due_date')
+            assigned_to = data.get('assigned_to')
+
+            task = Task.objects.create(title=title, description=description, due_date=due_date)
+            for emp_id in assigned_to:
+                employee = Employee.objects.get(id=emp_id)
+                task.assigned_to.add(employee)
+            
+            return HttpResponse(f'Task: {title} added successfully!')
+
+    # form = TaskForm(employees = employees)
     context = {'form':form}
     return render(request, 'create_task.html', context)
